ded4c2d29f6454cc363edc23c4ad8ebcb5c36f6a
Skianyan <99053383+Skianyan@users.noreply.github.com>
2023-02-20 19:10:07 -0800

    Update simpleLinkedList.js

--- Lista Enlazada Simple/src/simpleLinkedList.js
+++ Lista Enlazada Simple/src/simpleLinkedList.js
@@ -31,13 +31,29 @@ export default class SimpleLinkedList{
 // last.next = new nodo(data)
 // last = last.next
 
+
 // INSERTAR ENTRE DOS NODOS DE LA LISTA
 // La insercion de un nuevo nodo no se realiza siempre al principio (en cabeza/head)
 // de la lista o al final puede hacerse entre dos nodos cuales quiera de la lista
 
+// ALGORITMO:
 // 1. Crear un nodo con el nuevo elemento y el campo "enlace/next" a null. La referencia del
-// nodo se asigna a "nuevo/new"
+// nodo se asigna a "nuevo/new".
 // 2. Hacer que el campo enlace del nuevo nodo apunte al nodo "N2" ya que el nodo creado se
-// ubicará justo antes de N2
+// ubicará justo antes de N2.
 // 3. La variable referencia "anterior" tiene la direccion de N1 y eso exige hacer que anterior.enlace
-// apunte al nodo creado
+// apunte al nodo creado.
+
+// ELIMINACION DE UN NODO   
+// La operacion de eliminar un nodo de una lista enlazada supone enlazar el nodo anterior con el nodo
+// siguiente al que se desea eliminary liberar la memoria.
+
+// ALGORITMO:
+// 1. Busqueda del nodo que contiene el dato. Se ha de obtener la direccion del nodo a eliminar y la
+// direccion del anterior.
+// 2. El enlace del nodo anterior que apunte al siguiente nodo del cual se elimina.
+// 3. Si el nodo a eliminar es la cabeza de la lista (el primero), se modifica primero para que tenga
+// la direccion del siguiente nodo.
+// 4. La memoria del nodo se libera, dejando que todo esté referenciado.
+
+//
